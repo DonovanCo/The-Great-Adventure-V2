c52b1fa50056e264442256d4193123237ae5e554
cole.donovan <cole.donovan@MU-C3-10.mullen.pvt.k12.co.us>
2014-09-18 11:59:39 -0600

    began project

--- The Great Adventure V2/src/AdvRunner.java
+++ The Great Adventure V2/src/AdvRunner.java
@@ -1,10 +1,14 @@
-
+import java.util.Scanner;
 public class AdvRunner
 	{
 
 	public static void main(String[] args)
 		{
-		
+		Methods.beginAdv();
+		if(Methods.choice.equals("a"))
+			{
+				Methods.askFirst();
+			}
 
 		}
 
--- The Great Adventure V2/src/Methods.java
+++ The Great Adventure V2/src/Methods.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 
 public class Methods
 	{
+	static Scanner game = new Scanner(System.in);
+	public static String choice;
 
+	public static void beginAdv()
+		{
+		System.out
+				.println("Welcome to \"The Great Adventure\" (Version 2).\nYour adventure will begin shortly, but first some info about the game.");
+		System.out
+				.println("To choose an option use \"a\", \"b\" etc. \nIf you die there are no checkpoints.\nThere are 5 easter eggs in game. \nIf you can find them all you will receive a reward at the end.");
+		System.out.println(" Do you wish to a.)start or b.)");
+		choice = game.nextLine();
+		}
+
+	public static void askFirst()
+		{
+			System.out.println("You Awaken in a bed in a dark room. \n Do you a.) Go back to sleep b.) Look for a light source");
+			choice=game.nextLine();
+		}
+	public static void tellNoOption()
+		{
+			
+		}
 	}
